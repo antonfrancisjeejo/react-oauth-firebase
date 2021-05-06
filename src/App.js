2f192ffe3f02cf520492a43240df2f166a9c4b6e
antonfrancisjeejo <anton13jeejo@gmail.com>
2021-05-06 21:19:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { auth, provider, fbProvider, githubProvider } from "./firebase";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null);
+  const googleLogin = async () => {
+    try {
+      await auth.signInWithPopup(provider);
+      setUser(await auth.currentUser);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const facebookLogin = async () => {
+    try {
+      await auth.signInWithPopup(fbProvider);
+      setUser(await auth.currentUser);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const githubLogin = async () => {
+    try {
+      await auth.signInWithPopup(githubProvider);
+      setUser(await auth.currentUser);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log(user);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div>
+        <div className="buttonContainer" onClick={googleLogin}>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1200px-Google_%22G%22_Logo.svg.png"
+            alt=""
+          />
+          <button>Google Login</button>
+        </div>
+        <div className="buttonContainer" onClick={facebookLogin}>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png"
+            alt=""
+          />
+          <button>Facebook Login</button>
+        </div>
+        <div className="buttonContainer" onClick={githubLogin}>
+          <img
+            src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png"
+            alt=""
+          />
+          <button>Github Login</button>
+        </div>
+      </div>
+      <h1>{user?.email}</h1>
+    </section>
   );
-}
+};
 
 export default App;
